e71514929037901c010f69d54518f11cd2d33383
denlog1977 <denlog1977@gmail.com>
2019-10-22 16:47:34 +0300

    imageName

--- app/src/main/java/com/example/a122_endlessactivities/MainActivity.java
+++ app/src/main/java/com/example/a122_endlessactivities/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a122_endlessactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textViewImageName = (TextView) findViewById(R.id.textViewImageName);
+        String imageName = "";
+
+        Button buttonBack = (Button) findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewImageName.setText("Имя картинки " + imageName);
+            }
+        });
+
+        Button buttonForward = (Button) findViewById(R.id.buttonForward);
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewImageName.setText("Имя картинки " + imageName);
+            }
+        });
+
     }
+
 }
