ffcb7f64c37afffdc93bfceb4290235fde6393c1
denlog1977 <denlog1977@gmail.com>
2019-10-22 18:13:12 +0300

    Completed

--- app/src/main/java/com/example/a122_endlessactivities/MainActivity.java
+++ app/src/main/java/com/example/a122_endlessactivities/MainActivity.java
@@ -2,29 +2,34 @@ package com.example.a122_endlessactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.Random;
+
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    String imageName = "";
+    String imageName = "http://myfile.org/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         final TextView textViewImageName = (TextView) findViewById(R.id.textViewImageName);
+        textViewImageName.setText(imageName + Integer.toString(new Random().nextInt(100) + 1));
 
         Button buttonBack = (Button) findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageName = "НАЗАД";
-                textViewImageName.setText("Имя картинки " + imageName);
+                MainActivity.this.finish();
             }
         });
 
@@ -32,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         buttonForward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageName = "ВПЕРЁД";
-                textViewImageName.setText("Имя картинки " + imageName);
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
